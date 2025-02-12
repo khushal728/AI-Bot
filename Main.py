6bcd5616ff23e3713a89cdee4c88bee4838f5311
Khushal joshi <160943342+khushal728@users.noreply.github.com>
2025-02-12 16:57:58 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ from reportlab.pdfgen import canvas
 import pyperclip
 
 # Configure Gemini API
-genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+genai.configure(api_key=st.secrets["api"] ["GEMINI_API_KEY"])
 
 
 # Function to generate AI responses
